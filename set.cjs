993cd85493cbccbc11282ac90549aba3d1c26217
AKINDU11qwe <loadpakir9@gmail.com>
2024-11-24 09:54:03 +0530

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Gifted~2mYxXSbI#U9VZ4e5P4SPJmS11AbIjCVolBncAGvmlTHdpHYnAvm4",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~2uxR1ATa#MOAPYJTBi6OOKv94Lrk3iveLYL5ZRwPZFk_bYzUhHJ4",
   PREFIX: process.env.PREFIX || '.',
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking whatsapp status updates(stories)
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS !== undefined ? process.env.AUTO_READ_STATUS === 'true' : true,
